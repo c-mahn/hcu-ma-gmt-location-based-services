e55e9e52b1e2c663b0540310c6ebe93c4ce116b0
Christopher Mahn <56981804+c-mahn@users.noreply.github.com>
2022-05-26 21:03:10 +0200

    Update generate_trajectory.py

--- generate_trajectory.py
+++ generate_trajectory.py
@@ -21,7 +21,7 @@
 # import matplotlib.pyplot as plt
 # from scipy import interpolate
 # import numpy as np
-# import math as m
+import math as m
 # import sys
 # import os
 # from scipy.fft import fft, fftfreq
@@ -43,19 +43,53 @@ verbose = True  # Shows more debugging information
 
 class Trajectory():
     def __init__(self):
-        self.__position = {"x": 0.0, "y": 0.0}
-        self.__direction = 0
-        self.__total_length = 100
+        self.__position_init = {"x": 0.0, "y": 0.0}
+        # This is the initial position, where the trajectories are generated
+        # from
+
+        self.__direction_init = 0
+        # This is the initial direction the trajectory starts from
+
+        self.__direction_step_noise = 30/180*m.pi
+        # This is the amount of bending, that can occur for each point of the
+        # trajectory. The measurement is given in radians.
+
+        self.__length_total = 100
+        # length of the trajectory in footsteps
+
+        self.__length_step = 0.9
+        # length of the average footstep in meters
+
+        self.__length_step_noise = 0.15
+        # standard-deviation of the average footstep
+
+        self.__geometry = []
+        # This is a list of Line-Objects, that form a complex boundary for the
+        # trajectory
 
     def set_start_coordinate(self, x, y):
-        self.__position["x"] = float(x)
-        self.__position["x"] = float(y)
+        self.__position_init["x"] = float(x)
+        self.__position_init["x"] = float(y)
 
     def set_start_direction(self, direction):
-        self.__direction = float(direction)
+        self.__direction_init = float(direction)
 
     def set_trajectory_length(self, footsteps):
-        self.__total_length = float(footsteps)
+        self.__length_total = float(footsteps)
+
+    def set_median_step_size(self, length):
+        self.__length_step = float(length)
+
+    def set_step_size_noise(self, mdev):
+        self.__length_step_noise = float(mdev)
+
+
+class Line():
+    def __init__(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
 
 
 # Beginning of the Programm
